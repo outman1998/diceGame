1ddb3d723c86c8aec57d19efd4da13117cc27780
outman1998 <outmanmounir98@gmail.com>
2023-04-15 20:44:58 +0200

    all items selected
    items selected and ready to be used

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// player 0
+var player0 = document.querySelector('.player--0');
+const score0 = document.getElementById('score--0');
+const currentscore0 = document.getElementById('current--0');
+
+// player 1
+var player1 = document.querySelector('.player--1');
+const score1 = document.getElementById('score--1');
+const currentscore1 = document.getElementById('current--1')
+
+// buttons + dice
+const diceElement = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
