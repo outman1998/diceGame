b4302678def21da708107d6cf22a6cc74c32d01c
outman1998 <outmanmounir98@gmail.com>
2023-04-17 12:38:22 +0200

    moved the pictures to img folder

--- script.js
+++ script.js
@@ -43,3 +43,68 @@ var resetGame = function () {
 }
 // call the function so when page is reload, all the scores is set to default values 0.
 resetGame();
+
+
+
+// switch player function
+const switchPlayer = function() {
+
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0.classList.toggle('player--active');
+    player1.classList.toggle('player--active');
+
+}
+
+// rolling dice functionality
+btnRoll.addEventListener('click', function() {
+
+    if (playing) {
+        // 1. generating a random number from 1-6
+        const dice = Math.trunc(Math.random() * 6) + 1;
+
+        // 2. displays dice and hold-button
+        diceElement.classList.remove('hidden');
+
+        // her erstatter vi src koden til at være "dice-{tal mellem 1-6}" og på den måde får vi en random terning for hver klik.
+        diceElement.src = `images/dice-${dice}.png`;
+
+        // 3. check if dice is not 1
+        if(dice !== 1) {
+            // add dice to current score.
+            currentScore += dice;
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        } 
+        else {
+            switchPlayer();  
+        } 
+    }
+   
+});
+
+
+btnHold.addEventListener('click', function () {
+    
+    if(playing) {   
+        // 1. add current score to active players score
+        scores[activePlayer] += currentScore;
+
+        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+        // 2. check if score == 100 
+        if(scores[activePlayer] >= 100) {
+            playing = false;
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+            document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+            diceElement.classList.add('hidden');
+        } else {
+            switchPlayer();
+        }
+    }
+
+}); 
+
+
+
+btnNew.addEventListener('click', resetGame); 
