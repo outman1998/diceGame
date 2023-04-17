4e0666440a58ad8eb96b7a08e3b5d95636759fc8
outman1998 <outmanmounir98@gmail.com>
2023-04-17 12:32:05 +0200

    some functionality

--- script.js
+++ script.js
@@ -15,3 +15,31 @@ const diceElement = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+
+
+// erklærer disse variabler uden at sætte deres værdi
+let scores, currentScore, activePlayer, playing;
+
+// new game function
+var resetGame = function () {
+
+    // starting code
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+    
+    score0.textContent = 0;
+    score1.textContent = 0;
+    currentscore0.textContent = 0;
+    currentscore1.textContent = 0;
+    
+    diceElement.classList.add('hidden');
+    player0.classList.remove('player--winner');
+    player1.classList.remove('player--winner');
+    player0.classList.add('player--active');
+    player1.classList.remove('player--active');
+
+}
+// call the function so when page is reload, all the scores is set to default values 0.
+resetGame();
